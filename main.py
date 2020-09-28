2b275243890410f2723ea096e700859c76668e6d
Luís Antônio Granja <lagteixeira2015@gmail.com>
2020-09-28 17:25:18 -0300

    Classes

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 17:06:12 2020
+
+@author: luis
+"""
+
+class Cinema(Object):
+    def __init__(self, filial):
+        self.filial = filial
+
+class Administrador()
+    pass
+class Filme:
+    pass
+
+class Sessão:
+    pass
+
+class Administrador:
+    pass
+
+class Cliente:
+    
